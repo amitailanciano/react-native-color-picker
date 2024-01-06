dd9fa1ffc4d9ab48b1b26933628f3bbb87f5c0f5
Amitai Lanciano <tai@lanayo.com>
2024-01-05 19:42:42 -0700

    include dist

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export { fromHsv, toHsv } from './utils';
+export { HoloColorPicker as ColorPicker } from './HoloColorPicker';
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/typeHelpers.d.ts
@@ -0,0 +1,23 @@
+export declare type HsvColor = {
+    h: number;
+    s: number;
+    v: number;
+};
+export declare type Point2D = {
+    x: number;
+    y: number;
+};
+export interface IPickerProps {
+    color?: string | HsvColor;
+    defaultColor?: string | HsvColor;
+    oldColor?: string;
+    style?: any;
+    onColorSelected?: (selectedColor: string) => void;
+    onColorChange?: (selectedColor: HsvColor) => void;
+    onOldColorSelected?: (oldColor: string) => void;
+    hideSliders?: boolean;
+    onPickerStart?: () => void;
+    onPickerEnd?: () => void;
+    indicatorColorOverride?: string;
+}
+//# sourceMappingURL=typeHelpers.d.ts.map
